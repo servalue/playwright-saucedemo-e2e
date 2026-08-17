5c2cfd967638b8718388fa08afbcb9d2f4cd0289
Sergei <test@gmail.com>
2026-08-17 18:02:18 +0200

    Page object: login page tests and enhance error message handling

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,5 +1,5 @@
 class LoginPage {
- 
+
   constructor(page) {
     // Save the Playwright page object inside this class.
     this.page = page;
@@ -8,9 +8,16 @@ class LoginPage {
     this.usernameInput = page.getByPlaceholder('Username');
     this.passwordInput = page.getByPlaceholder('Password');
     this.loginButton = page.getByRole('button', { name: 'Login' });
+
+    this.errorMessage = page.locator('[data-test="error"]');
   }
 
-    // This method performs the complete login action.
+  // Open the login page.
+  async open() {
+    await this.page.goto('/');
+  }
+
+  // This method performs the complete login action.
   async login(username, password) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
